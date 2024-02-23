0339f701ea817d6d62671e124be9b293c2193b8f
noel gueco <156385766+catchspeed@users.noreply.github.com>
2024-02-23 08:01:44 +0800

    Update main.dart

--- main.dart
+++ main.dart
@@ -63,3 +63,68 @@ class InstagramScreen extends StatelessWidget {
                                   backgroundColor: Colors.grey,
                                   child: Center(
                                     child: Text(
+                                        '+',
+                                      style: TextStyle(
+                                        fontSize: 24,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          SizedBox(height: 4),
+                          Text(
+                            'Add',
+                            style: const TextStyle(fontSize: 12),
+                          ),
+                        ],
+                      ),
+                    );
+                  } else {
+                    // Rest of the items from the list
+                    List<String> names = [
+                      'Rhea',
+                      'Clarq',
+                      'Cheennee',
+                      'Ralf',
+                      'Nikhos',
+                      'Stephen',
+                      'Mark James',
+                      'PIPI Jones',
+                      'Russel',
+                      'Santiago'
+                    ];
+
+                    String userName = names[(index - 1) % names.length]; // Cycle through the list
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          Container(
+                            width: 60,
+                            height: 60,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              border: Border.all(color: Colors.orange, width: 2),
+                            ),
+                            child: CircleAvatar(
+                              radius: 28,
+                              backgroundColor: Colors.grey,
+                              backgroundImage: NetworkImage('https://picsum.photos/200?random=$index'),
+                            ),
+                          ),
+                          SizedBox(height: 4),
+                          Text(
+                            userName,
+                            style: const TextStyle(fontSize: 12),
+                          ),
+                        ],
+                      ),
+                    );
+                  }
+                },
+              ),
+            ),
